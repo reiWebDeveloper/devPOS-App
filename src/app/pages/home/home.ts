2b3b13d8f1c09d9a73a545bbc95d2fe4704532b8
reiAndroid <krajae265@gmail.com>
2025-10-28 10:22:41 +0100

    trying to fetch the data from JSON

--- src/app/pages/home/home.ts
+++ src/app/pages/home/home.ts
@@ -1,11 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Data } from '../../data';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [CommonModule],
+  standalone: true,
   templateUrl: './home.html',
   styleUrl: './home.scss',
 })
-export class Home {
+export class Home implements OnInit{
+[x: string]: any;
+  items: any[] = [];
 
+  constructor (private data: Data) {}
+
+  ngOnInit(): void {
+    this.data.getData().subscribe(
+      response=> {
+        console.log('Response from API:', response);
+      this.items = response.categories[0]?.products ?? [];
+      },
+      error=> console.error('Error fetching data:', error)
+    );
+  }
 }
