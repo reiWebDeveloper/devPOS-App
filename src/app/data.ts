6e255c944b111af8cc0498c8073467a6a23f7e7b
reiAndroid <krajae265@gmail.com>
2025-10-28 20:42:03 +0100

    added the data properly and style the UI

---
+++ src/app/data.ts
@@ -0,0 +1,16 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { MenuData } from './models/menu.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class Data {
+  private dataURL = "https://test.dev.al/test/";
+
+  constructor (private http : HttpClient) { }
+  getData(): Observable<MenuData> {
+    return this.http.get<MenuData>(this.dataURL);
+  }
+}
--- src/app/pages/home/home.ts
+++ src/app/pages/home/home.ts
@@ -1,11 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Data } from '../../data';
+import { CommonModule } from '@angular/common';
+import { Category } from '../../models/menu.model';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [CommonModule],
+  standalone: true,
   templateUrl: './home.html',
   styleUrl: './home.scss',
 })
-export class Home {
+export class Home implements OnInit{
+  categories: Category[] = [];
+  selectedCategory: Category |  null = null;
 
+  constructor (private data: Data) {}
+
+  ngOnInit(): void {
+    this.data.getData().subscribe({
+      next: (d)=> {
+        this.categories = d.categories;
+      },
+      error: (err)=> {
+        console.error('Error loading menu data:', err);
+      }
+    }
+      
+    );
+  }
+
+  selectCategory(category: Category): void {
+    this.selectedCategory = category;
+  }
 }
